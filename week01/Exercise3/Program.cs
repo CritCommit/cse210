9a898d60421b9afc6ceb1a883997f9b45742f04c
Kenton Beech <kbeech@byupathway.edu>
2026-09-06 20:27:08 +0200

    Completed

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 11);
+
+        int guess;
+
+        do
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher.");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower.");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+        } while (guess != magicNumber);
     }
 }
