63ca46deef1970e750f5de024f92f71eba478ed7
Kenton Beech <kbeech@byupathway.edu>
2026-09-07 23:23:57 +0200

    Completed

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+
+        string name = PromptUserName();
+        Console.WriteLine($"Hello {name}!");
+
+        int number = PromptUserNumber();
+        Console.WriteLine($"Your favorite number is {number}");
+
+        int squareNumber = SquareNumber(number);
+        Console.WriteLine($"Your number squared is {squareNumber}");
+    }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Enter your name: ");
+        return Console.ReadLine();
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Enter your favorite number: ");
+        return int.Parse(Console.ReadLine());
+    }
+
+    static int SquareNumber(int number)
+    {
+        return number * number;
     }
 }
